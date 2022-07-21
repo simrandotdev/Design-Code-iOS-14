fcf0037c292bb75bcf55a352dacc6f84e756c0a5
Simran Preet Singh <simran5590@gmail.com>
2022-07-20 22:06:55 -0700

    Initial Commit

---
+++ Shared/Design_Code_iOS_14App.swift
@@ -0,0 +1,17 @@
+//
+//  Design_Code_iOS_14App.swift
+//  Shared
+//
+//  Created by Simran Preet Narang on 2022-07-20.
+//
+
+import SwiftUI
+
+@main
+struct Design_Code_iOS_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
