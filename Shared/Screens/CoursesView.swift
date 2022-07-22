6a5c0df0d5ecf10d3b0e07c1de48ba430b4d1173
Simran Preet Singh <simran5590@gmail.com>
2022-07-22 07:31:55 -0700

    Transitioning between 2 views using Matched Geometry Effect

--- Shared/Screens/CoursesView.swift
+++ Shared/Screens/CoursesView.swift
@@ -11,18 +11,30 @@ struct CoursesView: View {
     
     @State var show = false
     
+    @Namespace var namespace
+    
     var body: some View {
         ZStack {
             CourseItem()
+                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
                 .frame(width: 335, height: 250)
             
-            VStack {
-                if show {
+            if show {
+                ScrollView {
                     CourseItem()
-                        .transition(.move(edge: .trailing))
-                        .ignoresSafeArea()
-                }
+                        .matchedGeometryEffect(id: "Card", in: namespace)
+                        .frame(height: 300)
+                        .transition(.slide)
+                    
+                    VStack {
+                        ForEach(0 ..< 25) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
+                }.ignoresSafeArea()
             }
+            
         }
         .onTapGesture {
             withAnimation(.spring()) {
