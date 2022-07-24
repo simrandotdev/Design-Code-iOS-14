823e66a2bdc3598175ecddfab6e8ab70992d26b3
Simran Preet Singh <simran5590@gmail.com>
2022-07-24 11:36:14 -0700

    Delay and Asymmetric Transition

--- Shared/Screens/CoursesView.swift
+++ Shared/Screens/CoursesView.swift
@@ -15,9 +15,17 @@ struct CoursesView: View {
     
     var body: some View {
         ZStack {
-            CourseItem()
-                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
-                .frame(width: 335, height: 250)
+            ScrollView {
+                VStack {
+                    CourseItem()
+                        .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
+                        .frame(width: 335, height: 250)
+                    
+                    CourseItem()
+                        .frame(width: 335, height: 250)
+                }
+                .frame(maxWidth: .infinity)
+            }
             
             if show {
                 ScrollView {
@@ -32,7 +40,15 @@ struct CoursesView: View {
                         }
                     }
                     .padding()
-                }.ignoresSafeArea()
+                }
+                .background(Color("Background 1"))
+                .transition(
+                    .asymmetric(
+                        insertion:  AnyTransition.opacity.animation(Animation.spring().delay(0.3)),
+                        removal: AnyTransition.opacity.animation(Animation.spring())
+                    )
+                )
+                .ignoresSafeArea()
             }
             
         }
